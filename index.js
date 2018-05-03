88bca63daf9eaa47c5a60f745e6cadb2c636ba9e
fcheng <fchengjin@126.com>
2018-05-03 17:59:45 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,76 @@
+// 使用webRTC获取用户当前ip
+// 创建RTCPeerConnection接口
+var localIP
+let conn = new RTCPeerConnection({
+  iceServers: []
+})
+let noop = function () {}
+conn.onicecandidate = function (ice) {
+  if (ice.candidate) {
+    // 使用正则获取ip
+    const ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
+    localIP = ipRegex.exec(ice.candidate.candidate)[1]
+    console.log(ice.candidate)
+    conn.onicecandidate = noop
+  }
+}
+// 随便创建一个叫狗的通道(channel)
+conn.createDataChannel('dog')
+// 创建一个SDP协议请求
+conn.createOffer(conn.setLocalDescription.bind(conn), noop)
+
+// 获取准确url
+var getUrl = function (url) {
+  var localReg = /^http(s)?:\/\/(localhost|127\.0\.0\.1)(:\d+)?\//i
+  if (localReg.test(url)) {
+    // 属于本地地址
+    var reg = /(localhost|127\.0\.0\.1)/
+    // 创建一个不断获取localIP的定时器, 当localIP有值时，返回
+    return new Promise(function (resolve, reject) {
+      var t = setInterval(function () {
+        if (localIP) {
+          clearInterval(t)
+          resolve(url.replace(reg, localIP))
+        }
+      }, 100)
+    })
+  } else {
+    return new Promise(function (resolve, reject) {
+      resolve(url)
+    })
+  }
+}
+// 创建二维码
+var createQrcode = function (ele, url) {
+  qrCode = new QRCode(ele, {
+    text: url,
+    width: 200,
+    height: 200,
+    colorDark: '#000000',
+    colorLight: '#ffffff',
+    correctLevel: QRCode.CorrectLevel.H
+  })
+}
+
+var init = function () {
+  var url
+  var qrCodeContainer, urlContainer
+  chrome.tabs.getSelected(function (tab) {
+    getUrl(tab.url).then(function (val) {
+      url = val
+    })
+  })
+  document.addEventListener('DOMContentLoaded', function (e) {
+    console.log(e)
+    qrCodeContainer = document.querySelector('.qrcode')
+    urlContainer = document.querySelector('.url')
+  })
+  var timer = setInterval(function () {
+    if (url && qrCodeContainer && urlContainer) {
+      urlContainer.innerHTML = url
+      createQrcode(qrCodeContainer, url)
+      clearInterval(timer)
+    }
+  }, 100)
+}
+init()
---
+++ readme.md
@@ -0,0 +1,11 @@
+# chrome生成当前网址二维码插件
+一键生成当前网址二维码，并自动将localhost,127.0.0.1转化为本机局域网ip，方便在真机上调试
+# 特点，使用webRTC获取浏览器ip
+参考[怎样用 JavaScript 程序获取客户的 IP 地址？ - Illgo的回答 - 知乎](https://www.zhihu.com/question/20675353/answer/335325619)
+# 使用方法
+- 下载`qrcode.crx`
+- 打开chrome扩展程序窗口（地址栏输入: `chrome://extensions`）
+- 将下载好的`qrcode.crx`拖进上面的窗口
+- 接下来就愉快的使用吧
+# 注意
+如果电脑开了代理，则获取到的二维码有可能不正确
