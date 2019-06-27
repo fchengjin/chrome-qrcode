ea99a20be3bc3f332bbf151cd51e4c6362f148ad
fcheng <fchengjin@126.com>
2019-06-27 15:00:16 +0800

    优化生成逻辑

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 使用webRTC获取用户当前ip
 // 创建RTCPeerConnection接口
-var localIP
+let localIP
 let conn = new RTCPeerConnection({
   iceServers: []
 })
@@ -20,24 +20,28 @@ conn.createDataChannel('dog')
 conn.createOffer(conn.setLocalDescription.bind(conn), noop)
 
 // 获取准确url
-var getUrl = function (url) {
-  var localReg = /^http(s)?:\/\/(localhost|127\.0\.0\.1|0(\.0){3})(:\d+)?\//i
+let getUrl = function (url) {
+  const localReg = /^http(s)?:\/\/(localhost|127\.0\.0\.1|0(\.0){3})(:\d+)?\//i
+  let count = 0;
   if (localReg.test(url)) {
     // 属于本地地址
     var reg = /(localhost|127\.0\.0\.1|0(\.0){3})/
     // 创建一个不断获取localIP的定时器, 当localIP有值时，返回
     return new Promise(function (resolve, reject) {
       var t = setInterval(function () {
+        count++
         if (localIP) {
           clearInterval(t)
           resolve(url.replace(reg, localIP))
         }
+        // 如果获取不到localIP则直接返回原来的ip
+        if (count > 19) {
+          resolve(url)
+        }
       }, 100)
     })
   } else {
-    return new Promise(function (resolve, reject) {
-      resolve(url)
-    })
+    return Promise.resolve(url)
   }
 }
 // 创建二维码
@@ -53,57 +57,57 @@ var createQrcode = function (ele, url) {
   })
 }
 
-var init = function () {
-  var url
-  var qrCodeContainer, urlContainer, copyBtn
-  chrome.tabs.getSelected(function (tab) {
-    getUrl(tab.url).then(function (val) {
-      url = val
+
+
+const getCurrentUrl = function () {
+  return new Promise(function (resolve) {
+    chrome.tabs.getSelected(async function (tab) {
+      console.log('tabSelected')
+      const tmp = await getUrl(tab.url)
+      resolve(tmp)
     })
   })
-  document.addEventListener('DOMContentLoaded', function (e) {
-    qrCodeContainer = document.querySelector('.qrcode')
-    appContainer = document.querySelector('.app')
-    urlContainer = document.querySelector('.url')
-    copyBtn = document.querySelector('.copy-btn')
-    toastContainer = document.querySelector('.toast')
-    var toast = {
-      show(text) {
-        toastContainer.innerText = text || '复制成功'
-        appContainer.classList.add('show-toast')
-        setTimeout(this.hide, 2000)
-      },
-      hide() {
-        appContainer.classList.remove('show-toast')
-      }
-    }
-    
-    // 回车重绘二维码
-    urlContainer.addEventListener('keydown', function(e) {
-      if(e.keyCode === 13) {
-        url = urlContainer.value;
-        createQrcode(qrCodeContainer, url)
-        urlContainer.blur()
-      }
-    })
-    copyBtn.addEventListener('click', function () {
-      urlContainer.select()
-      var bool = document.execCommand('copy', true)
-      if (bool) {
-        toast.show('复制成功')
-      } else {
-        toast.show('复制失败')
-      }
-      urlContainer.blur()
-    })
-  })
-  var timer = setInterval(function () {
-    if (url && qrCodeContainer && urlContainer) {
-      urlContainer.value = url
-      createQrcode(qrCodeContainer, url)
-      clearInterval(timer)
-    }
-  }, 100)
 }
 
-init()
+document.addEventListener('DOMContentLoaded', async function (e) {
+  console.log('domContentLoaded')
+  url = await getCurrentUrl()
+  const qrCodeContainer = document.querySelector('.qrcode')
+  const appContainer = document.querySelector('.app')
+  const urlContainer = document.querySelector('.url')
+  const copyBtn = document.querySelector('.copy-btn')
+  const toastContainer = document.querySelector('.toast')
+  const toast = {
+    show(text) {
+      toastContainer.innerText = text || '复制成功'
+      appContainer.classList.add('show-toast')
+      setTimeout(this.hide, 2000)
+    },
+    hide() {
+      appContainer.classList.remove('show-toast')
+    }
+  }
+
+  // 回车重绘二维码
+  urlContainer.addEventListener('keydown', async function (e) {
+    if (e.keyCode === 13) {
+      url = await getUrl(urlContainer.value);
+      console.log('url', url)
+      createQrcode(qrCodeContainer, url)
+      urlContainer.blur()
+      urlContainer.value = url
+    }
+  })
+  copyBtn.addEventListener('click', function () {
+    urlContainer.select()
+    var bool = document.execCommand('copy', true)
+    if (bool) {
+      toast.show('复制成功')
+    } else {
+      toast.show('复制失败')
+    }
+    urlContainer.blur()
+  })
+  urlContainer.value = url
+  createQrcode(qrCodeContainer, url)
+})
